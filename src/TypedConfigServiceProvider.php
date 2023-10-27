3f45ba9067e39edb89949b3f7717fbeb2194bda8
Marius <marius@coddin.nl>
2023-10-27 23:14:48 +0200

    Actually register the command

--- src/TypedConfigServiceProvider.php
+++ src/TypedConfigServiceProvider.php
@@ -5,11 +5,19 @@ declare(strict_types=1);
 namespace Coderg33k\TypedConfigGenerator;
 
 use Coderg33k\TypedConfigGenerator\Actions\GetClassForConfig;
+use Coderg33k\TypedConfigGenerator\Console\Commands\GenerateTypedConfig;
 use Coderg33k\TypedConfigGenerator\Helper\DoesTypedConfigClassExist;
 use Illuminate\Support\ServiceProvider;
 
 final class TypedConfigServiceProvider extends ServiceProvider
 {
+    public function boot(): void
+    {
+        $this->commands([
+            GenerateTypedConfig::class,
+        ]);
+    }
+
     public function register(): void
     {
         foreach (\array_keys(config()->all()) as $config) {
